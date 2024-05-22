8ecf64ead80d2a54371d4cdfe0681fa23ee170d2
Augustin Floren <augustinfloren@gmail.com>
2024-05-22 14:18:54 +0200

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import Blog from '../components/blog';
 
 export default function App() {
-  const [posts, setPosts] = useState([]);
-
-  const fetchPosts = () => {
-    axios
-      .get("http://localhost/osplaf/wp-json/wp/v2/posts")
-      .then((res) => {
-        console.log("ok")
-        setPosts(res.data);
-      });
-  }
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
   return (
     <div>
-      {posts.map((item) => (
-        <Blog
-          post={item}
-        />
-      ))}
     </div>
   )
 }
