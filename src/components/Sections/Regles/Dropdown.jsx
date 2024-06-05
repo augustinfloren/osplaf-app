fe7ee40cfdb889ed0db98eb3e45cd1c00658c496
Augustin Floren <augustinfloren@gmail.com>
2024-06-05 16:43:09 +0200

    Animation fonctionnelle : Ajout de hooks useRef, Reste à régler padding haut pour les step titles

--- src/components/Sections/Regles/Dropdown.jsx
+++ src/components/Sections/Regles/Dropdown.jsx
@@ -1,31 +1,38 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Regles.scss';
 import dropdownIcon from '../../../assets/icons/osplaf-dropdown.png';
 
 function Dropdown(rule) {
     const content = rule.content;
-    const [isOpen, setScrolldown] = useState(false);
+    const [open, setOpen] = useState(false);
+    const contentRef = useRef(null);
 
-    function toggleDropdown() {
-        setScrolldown(!isOpen);
+    const toggle = () => {
+        setOpen(!open);
     }
 
     return (
-        <div className={`regles__content__phase ${isOpen ? 'open' : ''}`} key={`regles__content__${content.name}`}>
+        <div className='regles__content__phase'
+            key={`regles__content__${content.name}`}
+            ref={contentRef} 
+            style={open ? { height: contentRef.current.scrollHeight + "px" } : { height: "max(8vw, 6rem)" }}
+        >
+
             <div className="regles__content__phase-title">
                 <h3>{content.name}</h3>
 
-                <div className={`regles__content__phase-title__dropdown ${isOpen ? 'open' : ''}`} onClick={toggleDropdown}>
+                <div className='regles__content__phase-title__dropdown' onClick={toggle}>
                     <img src={dropdownIcon} alt="Menu déroulant" />
                 </div>
             </div>
 
-            <div className={`regles__content__phase-steps ${isOpen ? 'open' : ''}`}>
+            <div className={`regles__content__phase-steps ${open ? "show" : ""}`} > 
                 {(Array.isArray(content.description)
                     ? content.description.map((step, index) => ( <div className='description-list' key={`${step}-${index}`}><div className='number'><h3>{index + 1}</h3></div><p>{step}</p></div>))
                     : <p className='description-single'>{content.description}</p>
                 )}
             </div>
+            
         </div>
     )
 }
