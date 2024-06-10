945746ed7113d2f31049ff9a6b77cfee51be0c4b
Augustin Floren <augustinfloren@gmail.com>
2024-06-10 15:55:15 +0200

    Suppression de l'animation de hauteur. Simple effet d'opacité sur contenu

--- src/components/Sections/Regles/Dropdown.jsx
+++ src/components/Sections/Regles/Dropdown.jsx
@@ -1,10 +1,11 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './Regles.scss';
 import dropdownIcon from '../../../assets/icons/osplaf-dropdown.png';
 
 function Dropdown(rule) {
     const content = rule.content;
     const [open, setOpen] = useState(false);
+    const [showSteps, setShowSteps] = useState(false);
     const contentRef = useRef(null);
 
     const openPhaseContainer = (event) => {
@@ -20,12 +21,23 @@ function Dropdown(rule) {
         }
     }
 
+    useEffect(() => {
+        if (open) {
+            const timeoutId = setTimeout(() => {
+                setShowSteps(true);
+            }, 100);
+
+            return () => clearTimeout(timeoutId);
+        } else {
+            setShowSteps(false);
+        }
+    }, [open]);
+
     return (
-        <div className='regles__content__phase'
+        <div className={`regles__content__phase ${open ? "open" : ""}`}
             key={`regles__content__${content.name}`}
             onClick={openPhaseContainer}
             ref={contentRef} 
-            style={open ? { height: contentRef.current.scrollHeight + "px" } : { height: "max(8vw, 6rem)" }}
         >
 
             <div className="regles__content__phase-title"  onClick={openChildPhaseContainer}>
@@ -34,12 +46,14 @@ function Dropdown(rule) {
                 <img className={`regles__content__phase-title__dropdown ${open ? "open" : ""}`} src={dropdownIcon} alt="Menu déroulant" onClick={openChildPhaseContainer} ref={contentRef} />
             </div>
 
-            <div className={`regles__content__phase-steps ${open ? "show" : ""}`} > 
-                {(Array.isArray(content.description)
-                    ? content.description.map((step, index) => ( <div className='description-list' key={`${step}-${index}`}><div className='number'><h3>{index + 1}</h3></div><p>{step}</p></div>))
-                    : <p className='description-single'>{content.description}</p>
-                )}
-            </div>
+            {open &&
+                <div className={`regles__content__phase-steps ${showSteps ? "show" : ""}`} > 
+                    {(Array.isArray(content.description)
+                        ? content.description.map((step, index) => ( <div className='description-list' key={`${step}-${index}`}><div className='number'><h3>{index + 1}</h3></div><p>{step}</p></div>))
+                        : <p className='description-single'>{content.description}</p>
+                    )}
+                </div>
+            }
             
         </div>
     )
