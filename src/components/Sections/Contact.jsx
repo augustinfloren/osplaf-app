ed84ed94066eb9b23cb929e6606cafc8f1cefaa8
Augustin Floren <augustinfloren@gmail.com>
2024-06-11 15:40:16 +0200

    formulaire de contact en place

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -10,6 +10,15 @@ function Contact() {
                 <div className="contact-content__form">
                     <img src={arrow3} alt="flêche" className='arrow3'/>
                     <h2>Contact</h2>
+
+                    <form className='contact-form'>
+                        <input className='contact-form__name' placeholder='Nom'></input>
+                        <input className='contact-form__mail' placeholder='Mail'></input>
+                        <input className='contact-form__objet' placeholder='Objet'></input>
+                        <div className='contact-form__message'>
+                            <textarea placeholder='Message'></textarea>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
