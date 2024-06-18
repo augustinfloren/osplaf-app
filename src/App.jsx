b0f484c87b5078fc16f86fe9f12c8df9ce65f641
Augustin Floren <augustinfloren@gmail.com>
2024-06-18 17:20:27 +0200

    Suppression de la route shop

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 
 import Home from './components/Home/Home';
-import Shop from './components/Shop/Shop';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home/>
     },
-    {
-      path: '/shop',
-      element: <Shop />
-    }
 ]);
 
 function App()  {
--- src/components/Shop/Shop.jsx
+++
@@ -1,8 +0,0 @@
-import ShopifyBtn from './ShopifyBtn'
-
-export default function Shop() {
-    return (
-            <h1></h1>
-        </div>
-    )
-}
