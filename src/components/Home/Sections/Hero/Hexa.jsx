6c24aa94be07445c5263969b7f4352645210d27d
Augustin Floren <augustinfloren@gmail.com>
2024-06-22 10:52:20 +0200

    Anim hexa OK

--- src/components/Home/Sections/Hero/Hexa.jsx
+++ src/components/Home/Sections/Hero/Hexa.jsx
@@ -2,31 +2,59 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { hexa } from '../../../../assets/index';
 
-const duration = 4;
+const duration = 2;
 
 function Hexa() {
-
-
-
+    const [cycle, setCycle] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCycle(cycle => cycle + 1);
+      }, duration * 1000); 
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    const containerVariants = {
+      hidden: { 
+        opacity: 0,
+      },
+      show: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.2,
+        }
+      }
+    };
+  
+    const childrenVariants = {
+      hidden: { 
+        opacity: 0,
+     },
+      show: { 
+        opacity: 1,
+     }
+    };
+  
     return (
-        <div className="hexa-container">
-            {hexa.map((shape, i) => (
-                <motion.img
-                    key={shape.id}
-                    className={`hexa ${shape.id}`}
-                    src={shape.src}
-                    alt={shape.alt}
-                    animate={{opacity: [0, 1, 1, 0]}}
-                    transition={{
-                        ease: "linear",
-                        repeat: Infinity,
-                        repeatType: "loop",
-                        duration: 4,
-                    }}
-                />
-            ))}
-        </div>
+      <motion.div 
+        key={cycle} // Ajoute une clé pour réinitialiser l'animation à chaque cycle
+        className="hexa-container"
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+      >
+        {hexa.map((shape, i) => (
+          <motion.img
+            key={shape.id}
+            className={`hexa ${shape.id}`}
+            src={shape.src}
+            alt={shape.alt}
+            variants={childrenVariants}
+          />
+        ))}
+      </motion.div>
     );
-}
-
-export default Hexa;
+  }
+  
+  export default Hexa;
