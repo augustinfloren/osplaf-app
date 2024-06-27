c450c600d65e4b0ac2dbb63e7be5b830f9f9ccd1
Augustin Floren <augustinfloren@gmail.com>
2024-06-27 16:59:16 +0200

    test .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+// test
 
 import Home from './components/Home/Home';
 import Cgv from './components/Cgv/Cgv';
